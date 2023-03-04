46013623c6fd2dc19891b7c89d20c21714404e7b
Simon Julian Lauw <simonjulian21@gmail.com>
2023-03-04 14:52:12 +0800

    Finish angel n mortal bot

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import numpy as np
 import pandas as pd
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 filename = "user.csv"
+group_chat_id = None
+initial_message_id = None
+initial_message = ["Let's join ICy Angel and Mortal :)"]
+
+
+def generate_message():
+    global initial_message
+    message = initial_message[0] + "\n"
+    for index, username in enumerate(initial_message[1:], start=1):
+        message += f"{index}. {username}\n"
+    return message
 
 
 class Person:
@@ -39,16 +51,40 @@ def get_username_id_from_update(update: Update) -> (str, str):
     return {'username': username, 'id': chat_id}
 
 
+def get_id_from_group(update: Update) -> str:
+    return update.message.chat.id
+
+
+async def add_to_group(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global group_chat_id
+    group_chat_id = get_id_from_group(update)
+    await update.message.reply_text("Adding the bot successful!")
+
+
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     data = get_username_id_from_update(update)
     if update.message.chat.type == 'group':  # Group is not supposed to join
-        await update.message.reply_text("Don't be naughty!")
+        await update.message.reply_text("Group cannot join this game, don't be naughty!")
         return
 
     p = Person(data['username'], data['id'])
     p.writeToCsv()
+    if any(filter(lambda x: p.name == x,
+                  initial_message)):
+        await update.message.reply_text("You have registered, please don't register twice >:(")
+        return
 
+    initial_message.append(p.name)
+    await context.bot.edit_message_text(chat_id=group_chat_id, message_id=initial_message_id,
+                                        text=generate_message())
     await update.message.reply_text("Registration Successful!")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await add_to_group(update, context)
+    global initial_message_id
+    result = await context.bot.send_message(chat_id=group_chat_id, text=generate_message())
+    initial_message_id = result.message_id
 
 
 async def send_message(data, update, context, option: str):
@@ -56,23 +92,23 @@ async def send_message(data, update, context, option: str):
 
     try:
         df = pd.read_csv(filename).set_index('name')
-        username = data['username']
-        if username not in df.index:
+        my_username = data['username']
+        if my_username not in df.index:
             await update.message.reply_text("You haven't registered yet naughty kid :(")
             return
 
-        username = df.loc[[username]][option].values[0]
-        if username is None:
+        other_username = df.loc[[my_username]][option].values[0]
+        if pd.isnull(other_username):
             await update.message.reply_text("Be patient :( You haven't been assigned a mortal/angel yet")
             return
 
         try:
-            chat_id = df.loc[[username]]['id'].values[0]
-            await context.bot.send_message(chat_id=str(chat_id),
-                                           text=f"A message from your {'angel' if option == 'mortal' else 'mortal'}: \n\n{message}")
+            await context.bot.send_message(chat_id=group_chat_id,
+                                           text=f"A message from {other_username if option == 'mortal' else my_username} {'angel' if option == 'mortal' else 'mortal'}: \n\n{message}")
         except Exception:
-            await update.message.reply_text("Your mortal/angel is naughty, he/she hasn't registered ye")
-    except Exception:
+            await update.message.reply_text("Seems that I cannot find the group :(, please register your group")
+    except Exception as e:
+        print(e)
         await update.message.reply_text("Something goes wrong :(")
 
 
@@ -117,11 +153,22 @@ async def generate_pairing(update: Update, context: ContextTypes.DEFAULT_TYPE):
     df.to_csv(filename, sep=",")
 
 
+async def help_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("To use me\n1. Add me to the group that you wanna play on\n"
+                                    "2. call /start in that group\n"
+                                    "3. For each player, send /register in private message to the bot!\n"
+                                    "4. Wait to be paired up >:(\n"
+                                    "5. Send each other well wishes using /sendangel and /sendmortal to send to your angel and mortal respectively!!\n\n"
+                                    "Alrighty, have fun bois and gals :)")
+
+
 if __name__ == "__main__":
     token = "6006404430:AAHTpuocTUHrQWw9hjIJVINhO4U0PO-aVhI"
 
     application = Application.builder().token(token).build()
 
+    application.add_handler(CommandHandler("help", help_message))
+    application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("register", register))
     application.add_handler(CommandHandler("sendmortal", send_mortal))
     application.add_handler(CommandHandler("sendangel", send_angel))
